428899a15805a1bc113be99c01b630f558762b18
Trung Kien <trungkien24072005@gmail.com>
2026-08-19 09:34:06 +0700

    w

--- frontend_mobile/lib/core/contants/url_config.dart
+++ frontend_mobile/lib/core/contants/url_config.dart
@@ -1,6 +1,6 @@
 class UrlConfig {
-  static const String baseUrl = 'https://web-app-glasses.onrender.com';
-  static const String backendBaseUrl = 'https://web-app-glasses.onrender.com/'; // cho VNPay WebView
+  static const String baseUrl = 'https://web-app-glasses.liquid-phim.workers.dev';
+  static const String backendBaseUrl = 'https://web-app-glasses.liquid-phim.workers.dev/'; // cho VNPay WebView
   // static const String baseUrl = 'http://localhost:8017';
   // static const String frontendBaseUrl = 'http://localhost:8080'; // cho VNPay WebView
   static const String refreshToken = '$baseUrl/auth/refresh'; 
